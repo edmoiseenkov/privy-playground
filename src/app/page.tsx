de161bbab31d8c58da7f77a9c7fc5b6246262a9a
edmoiseenkov <edmoiseenkov@gmail.com>
2023-06-08 18:10:46 +0300

    add write transaction hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 'use client'
 
-import { Box, Button, Center, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Container, Flex, Heading, Text, VStack } from '@chakra-ui/react';
 import { usePrivy } from '@privy-io/react-auth';
-import React  from 'react';
+import Editor from '@monaco-editor/react';
+import React, { useMemo } from 'react';
+import { utils } from 'ethers';
+
 import { useContractRead } from '@/hooks/use-contract-read';
 import ERC20 from '@/ERC20.json';
+import { useContractWrite } from '@/hooks/use-contract-write';
 
 export default function Home() {
-  const { ready, authenticated, user, login } = usePrivy();
+  const { ready, authenticated, user, login, logout } = usePrivy();
 
   const isLoggedIn = ready && authenticated;
 
@@ -19,12 +23,57 @@ export default function Home() {
     args: ['0xC1A98a78411A9CC71c526EAD8b922a841baF7deD'],
   });
 
+  const {
+    write: approve,
+    isLoading: isApproving,
+  } = useContractWrite({
+    abi: ERC20.abi,
+    address: '0x073eF624e2a2fB5e24BC047484c22A7c44e2c9CB',
+    functionName: 'approve',
+  })
+
+  const formattedUserDAI = useMemo(() => {
+    if (!userDAI) {
+      return 0;
+    }
+
+    return +utils.formatUnits(userDAI.toString());
+  }, [userDAI]);
+
   return isLoggedIn ? (
-    <Box>
-      <Heading>Account page</Heading>
-      <Text>DAI: {userDAI}</Text>
-      <Text whiteSpace={'pre-wrap'}>{JSON.stringify(user, null, 2)}</Text>
-    </Box>
+    <Container p={'20px'}>
+      <VStack align={'stretch'}>
+        <Flex align={'center'} justify={'space-between'}>
+          <Heading>Account page</Heading>
+          <Button size={'sm'} onClick={() => logout()}>Logout</Button>
+        </Flex>
+
+        <Button
+          w={'fit-content'}
+          isLoading={isApproving}
+          onClick={() => approve(['0x82095Bf9586FF437B1f3915509E27Fdf191814f8', 1])}
+        >Approve</Button>
+
+        <Text>
+          <Box as={'span' as any} fontWeight={700}>DAI Balance: </Box>
+          {formattedUserDAI}
+        </Text>
+        {/*<Text whiteSpace={'pre-wrap'}>{JSON.stringify(user, null, 2)}</Text>*/}
+        <Editor
+          height="500px"
+          defaultLanguage="json"
+          value={JSON.stringify(user, null, 2)}
+          theme="onedark"
+          options={{
+            readOnly: true,
+            minimap: {
+              enabled: false,
+            },
+            scrollBeyondLastLine: false,
+          }}
+        />
+      </VStack>
+    </Container>
   ) : (
     <Center minH={'100vh'}>
       <Button isLoading={!ready} onClick={login}>
--- src/hooks/use-contract-read.ts
+++ src/hooks/use-contract-read.ts
@@ -1,6 +1,6 @@
 import { Contract } from 'ethers';
 import { useQuery } from 'react-query'
-import { usePrivy } from '@privy-io/react-auth';
+import { usePrivy, useWallets } from '@privy-io/react-auth';
 
 interface IUsePrepareContractReadOptions  {
   address: string;
@@ -14,46 +14,38 @@ interface IUsePrepareContractReadOptions  {
 export const useContractRead = (options: IUsePrepareContractReadOptions) => {
   const { address, abi, functionName, args = [], enabled = true, onSuccess } = options;
 
-  const { user, getEthersProvider, signMessage, walletConnectors } = usePrivy();
+  const { user } = usePrivy();
+  const { wallets } = useWallets()
 
   return useQuery({
     retry: false,
     refetchOnWindowFocus: false,
     refetchOnMount: false,
     refetchOnReconnect: false,
-    enabled: enabled && !!walletConnectors && !!user?.wallet?.address,
+    enabled: enabled && !!user && !!wallets.length,
     queryKey: [address, functionName, args],
     queryFn: async () => {
+      // await signMessage('hello world');
 
-      await signMessage('hello world');
+      const embeddedWallet = wallets.find((wallet) => wallet.address === user!.wallet?.address);
 
-      const embeddedWallet = walletConnectors!.walletConnectors.find((wallet) => wallet.walletClientType === 'privy');
-
-      console.log('embeddedWallet', embeddedWallet)
-
-      await embeddedWallet!.connect({
-        showPrompt: false,
-        chainId: 0x66EED
-      });
-
-      await walletConnectors?.setActiveWallet(embeddedWallet.wallets[0]!.address);
-
-      console.log(embeddedWallet);
-
-      const provider = getEthersProvider();
-
-      const network = await provider.getNetwork();
-
-      console.log('network', network);
-
-      if (!provider || !user?.wallet?.address) {
+      if (!embeddedWallet) {
         return;
+      }
+
+      const provider = await embeddedWallet.getEthersProvider();
+
+      if (embeddedWallet.walletClientType === 'privy') {
+        await provider.send(
+          'wallet_switchEthereumChain',
+          ['0x66EED'],
+        )
       }
 
       const contract = new Contract(
         address,
         abi,
-        provider.getSigner(user.wallet.address) as any,
+        provider as any,
       );
 
       const fn = contract[functionName];
